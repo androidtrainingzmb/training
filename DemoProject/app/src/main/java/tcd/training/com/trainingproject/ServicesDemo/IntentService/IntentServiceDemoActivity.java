a3e57fe25766821f4e6d5ac9ea6903e9c47630b4
CPU10661-local <dungchoviechoc@gmail.com>
2017-08-01 16:03:57 +0700

    add demos - add EventBus method demo - add RxJava method demo

--- DemoProject/app/src/main/java/tcd/training/com/trainingproject/ServicesDemo/IntentService/IntentServiceDemoActivity.java
+++ DemoProject/app/src/main/java/tcd/training/com/trainingproject/ServicesDemo/IntentService/IntentServiceDemoActivity.java
@@ -1,26 +1,34 @@
 package tcd.training.com.trainingproject.ServicesDemo.IntentService;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.nfc.tech.IsoDep;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 import tcd.training.com.trainingproject.R;
+import tcd.training.com.trainingproject.ServicesDemo.IntentService.EventBus.EventBusMessage;
+import tcd.training.com.trainingproject.ServicesDemo.IntentService.EventBus.IntentServiceWithEventBus;
+import tcd.training.com.trainingproject.ServicesDemo.IntentService.LocalBroadcastReceiver.IntentServiceWithBroadcastReceiver;
+import tcd.training.com.trainingproject.ServicesDemo.IntentService.ResultReceiver.IntentServiceWithResultReceiver;
+import tcd.training.com.trainingproject.ServicesDemo.IntentService.ResultReceiver.MyResultReceiver;
+import tcd.training.com.trainingproject.ServicesDemo.IntentService.RxJava.IntentServiceWithRxJava;
+import tcd.training.com.trainingproject.ServicesDemo.IntentService.RxJava.RxBus;
 
 public class IntentServiceDemoActivity extends AppCompatActivity {
 
@@ -28,8 +36,10 @@ public class IntentServiceDemoActivity extends AppCompatActivity {
 
     public static final int RESULT_RECEIVER_METHOD = 0;
     public static final int BROADCAST_RECEIVER_METHOD = 1;
+    public static final int EVENT_BUS_METHOD = 2;
+    public static final int RX_JAVA_METHOD = 3;
 
-    private TextView integerValueTextView;
+    private TextView mIntegerValueTextView;
     private int mInteger = 0;
 
     private MyResultReceiver mResultReceiver;
@@ -41,11 +51,14 @@ public class IntentServiceDemoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_service_demo);
 
-        integerValueTextView = findViewById(R.id.tv_integer_value);
+        mIntegerValueTextView = findViewById(R.id.tv_integer_value);
 
         mCurrentMethod = getIntent().getIntExtra(getString(R.string.integer_type), RESULT_RECEIVER_METHOD);
-        if (mCurrentMethod == BROADCAST_RECEIVER_METHOD) {
-            ((RadioButton)findViewById(R.id.rb_broadcast_receiver)).setChecked(true);
+        switch (mCurrentMethod) {
+            case RESULT_RECEIVER_METHOD: ((RadioButton)findViewById(R.id.rb_result_receiver)).setChecked(true); break;
+            case BROADCAST_RECEIVER_METHOD: ((RadioButton)findViewById(R.id.rb_broadcast_receiver)).setChecked(true); break;
+            case EVENT_BUS_METHOD: ((RadioButton)findViewById(R.id.rb_event_bus)).setChecked(true); break;
+            case RX_JAVA_METHOD: ((RadioButton)findViewById(R.id.rb_rx_java)).setChecked(true); break;
         }
 
         setupServiceResultReceiver();
@@ -60,7 +73,7 @@ public class IntentServiceDemoActivity extends AppCompatActivity {
             public void onReceivedResult(int resultCode, Bundle resultData) {
                 if (resultCode == Activity.RESULT_OK) {
                     mInteger = resultData.getInt(getString(R.string.integer_type));
-                    integerValueTextView.setText(String.valueOf(mInteger));
+                    mIntegerValueTextView.setText(String.valueOf(mInteger));
                 }
             }
         });
@@ -69,31 +82,61 @@ public class IntentServiceDemoActivity extends AppCompatActivity {
             @Override
             public void onReceive(Context context, Intent intent) {
                 mInteger = intent.getIntExtra(getString(R.string.integer_type), -1);
-                integerValueTextView.setText(String.valueOf(mInteger));
+                mIntegerValueTextView.setText(String.valueOf(mInteger));
             }
         };
+        // RxJava
+        RxBus.subscribe(new Consumer<EventBusMessage>() {
+            @Override
+            public void accept(@NonNull final EventBusMessage eventBusMessage) throws Exception {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mInteger = eventBusMessage.getInteger();
+                        mIntegerValueTextView.setText(String.valueOf(mInteger));
+                    }
+                });
+            }
+        });
     }
 
-    // register and unregister broadcast receiver
+    // register and unregister broadcast receiver / event bus
     @Override
     protected void onResume() {
         super.onResume();
         LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver, new IntentFilter(IntentServiceWithBroadcastReceiver.INTENT_ACTION));
+        EventBus.getDefault().register(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe (threadMode = ThreadMode.MAIN)
+    public void onMessage(EventBusMessage event) {
+        mInteger = event.getInteger();
+        mIntegerValueTextView.setText(String.valueOf(mInteger));
     }
 
     private void launchService() {
         Intent serviceIntent = new Intent();
-        if (mCurrentMethod == RESULT_RECEIVER_METHOD) {
-            serviceIntent.setClass(this, IntentServiceWithResultReceiver.class);
-            serviceIntent.putExtra(getString(R.string.result_receiver), mResultReceiver);
-        } else {
-            serviceIntent.setClass(this, IntentServiceWithBroadcastReceiver.class);
+        switch (mCurrentMethod) {
+            case RESULT_RECEIVER_METHOD:
+                serviceIntent.setClass(this, IntentServiceWithResultReceiver.class);
+                serviceIntent.putExtra(getString(R.string.result_receiver), mResultReceiver);
+                break;
+            case BROADCAST_RECEIVER_METHOD:
+                serviceIntent.setClass(this, IntentServiceWithBroadcastReceiver.class);
+                break;
+            case EVENT_BUS_METHOD:
+                serviceIntent.setClass(this, IntentServiceWithEventBus.class);
+                break;
+            case RX_JAVA_METHOD:
+                serviceIntent.setClass(this, IntentServiceWithRxJava.class);
+                break;
         }
         serviceIntent.putExtra(getString(R.string.integer_type), mInteger);
         startService(serviceIntent);
@@ -110,6 +153,12 @@ public class IntentServiceDemoActivity extends AppCompatActivity {
                     break;
                 case R.id.rb_result_receiver:
                     mCurrentMethod = RESULT_RECEIVER_METHOD;
+                    break;
+                case R.id.rb_event_bus:
+                    mCurrentMethod = EVENT_BUS_METHOD;
+                    break;
+                case R.id.rb_rx_java:
+                    mCurrentMethod = RX_JAVA_METHOD;
                     break;
             }
             launchService();
