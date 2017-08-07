bd4ca8cfbe02c4342e0557c0189b89fa701f7d65
CPU10661-local <dungchoviechoc@gmail.com>
2017-08-07 11:45:47 +0700

    minor update - update to an easier to understand demonstration

--- DemoProject/app/src/main/java/tcd/training/com/trainingproject/ImagesTopic/CacheImageUsingMemoryDemoActivity.java
+++ DemoProject/app/src/main/java/tcd/training/com/trainingproject/ImagesTopic/CacheImageUsingMemoryDemoActivity.java
@@ -4,29 +4,50 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v4.util.LruCache;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Calendar;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
 import tcd.training.com.trainingproject.R;
 
 public class CacheImageUsingMemoryDemoActivity extends AppCompatActivity {
 
-    private static final int NUMBER_OF_IMAGES = 100;
+    // for caching
+    private static final int NUMBER_OF_IMAGES = 5;
     private LruCache<String, Bitmap> mMemoryCache;
     private boolean mIsUsingCache;
 
+    // layout
     private LinearLayout mRootLinearLayout;
-    private Switch mUsingCacheSwitch;
     private TextView mShownImagesTextView;
+
+    // loading info
+    private long mStartTime;
+    private int mImageIndex = 0;
+    private String[] mImagesUrl = new String[] {
+            "https://source.unsplash.com/19NtUg2HjeQ/",
+            "https://source.unsplash.com/MUHe6nNMXVI/",
+            "https://source.unsplash.com/_Ajm-ewEC24/",
+            "https://source.unsplash.com/mtu6m_nLFQI/",
+            "https://source.unsplash.com/8Hjx3GNZYeA/"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +60,20 @@ public class CacheImageUsingMemoryDemoActivity extends AppCompatActivity {
         loadAllImages();
     }
 
+    private void increaseImageIndex() {
+        mImageIndex++;
+        mShownImagesTextView.setText(mImageIndex + "/" + NUMBER_OF_IMAGES);
+        if (mImageIndex == NUMBER_OF_IMAGES) {
+            long end = Calendar.getInstance().getTimeInMillis();
+            mShownImagesTextView.append(" (" + String.valueOf(end - mStartTime) + "ms)");
+        }
+    }
+
     private void initializeUiComponents() {
         mRootLinearLayout = findViewById(R.id.linear_layout_images_list);
         mShownImagesTextView = findViewById(R.id.tv_shown_images);
 
-        mUsingCacheSwitch = findViewById(R.id.sw_using_cache);
+        Switch mUsingCacheSwitch = findViewById(R.id.sw_using_cache);
         mUsingCacheSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
@@ -56,14 +86,15 @@ public class CacheImageUsingMemoryDemoActivity extends AppCompatActivity {
     }
 
     private void loadAllImages() {
+        // clean previous process
         mRootLinearLayout.removeAllViews();
-        long start = Calendar.getInstance().getTimeInMillis();
+        mShownImagesTextView.setText("0/" + NUMBER_OF_IMAGES);
+        mImageIndex = 0;
+        mStartTime = Calendar.getInstance().getTimeInMillis();
+        // start loading
         for (int i = 0; i < NUMBER_OF_IMAGES; i++) {
-            loadBitmap(R.drawable.landscape_large, createAnImageView());
-            mShownImagesTextView.setText(String.valueOf(i + 1));
+            loadBitmap(mImagesUrl[i % mImagesUrl.length], createAnImageView());
         }
-        long end = Calendar.getInstance().getTimeInMillis();
-        mShownImagesTextView.append(" (" + String.valueOf(end - start) + "ms)");
     }
 
     private void prepareToCache() {
@@ -87,67 +118,66 @@ public class CacheImageUsingMemoryDemoActivity extends AppCompatActivity {
         return mMemoryCache.get(key);
     }
 
-    private void loadBitmap(int resId, ImageView imageView) {
+    private void loadBitmap(String imageUrl, ImageView imageView) {
         if (mIsUsingCache) {
-            final Bitmap bitmap = getBitmapFromMemoryCache(String.valueOf(resId));
+            final Bitmap bitmap = getBitmapFromMemoryCache(imageUrl);
             // it's already in the cache memory
             if (bitmap != null) {
                 imageView.setImageBitmap(bitmap);
+                increaseImageIndex();
             } else {
-                imageView.setImageResource(resId);
+                loadImageUsingDecodeStream(imageUrl);
                 CacheBitmapTask task = new CacheBitmapTask();
-                task.execute(resId);
+                task.execute(imageUrl);
             }
         } else {
-            imageView.setImageResource(resId);
+            loadImageUsingDecodeStream(imageUrl);
         }
     }
 
-    private class CacheBitmapTask extends AsyncTask<Integer, Void, Bitmap> {
+    private class CacheBitmapTask extends AsyncTask<String, Void, Bitmap> {
 
         @Override
-        protected Bitmap doInBackground(Integer... integers) {
-            final Bitmap bitmap = decodeBitmapFromResource(getResources(), integers[0], 100, 100);
-            addBitmapToMemoryCache(String.valueOf(integers[0]), bitmap);
+        protected Bitmap doInBackground(String... strings) {
+//            final Bitmap bitmap = decodeBitmapFromResource(getResources(), strings[0], 100, 100);
+            Bitmap bitmap = null;
+            try {
+                bitmap = Glide.with(CacheImageUsingMemoryDemoActivity.this).asBitmap().load(strings[0]).into(500, 500).get();
+                addBitmapToMemoryCache(strings[0], bitmap);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
             return bitmap;
         }
     }
 
-    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        int inSampleSize = 1;
-        if (options.outHeight > reqHeight || options.outWidth> reqWidth) {
-
-            // don't know why we have to divide them by 2 but it's declared like this in documentation
-            final int halfHeight = options.outHeight / 2;
-            final int halfWidth = options.outWidth / 2;
-
-            /*
-             * Calculate the largest inSampleSize value that is a power of 2 and keeps both
-             * height and width larger than the requested height and width.
-             * (A power of two value is calculated because the decoder uses a final value by rounding down to the nearest power of two,
-             * as per the inSampleSize documentation)
-             */
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
+    private void loadImageUsingDecodeStream(final String imageUrl) {
+        final ImageView imageView = createAnImageView();
+        HandlerThread handlerThread = new HandlerThread(getPackageName());
+        handlerThread.start();
+        Handler handler = new Handler(handlerThread.getLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(imageUrl);
+                    final Bitmap bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageBitmap(bitmap);
+                            increaseImageIndex();
+                        }
+                    });
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-
-        return inSampleSize;
-    }
-
-    private Bitmap decodeBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
+        });
     }
 
     private ImageView createAnImageView() {
@@ -160,4 +190,41 @@ public class CacheImageUsingMemoryDemoActivity extends AppCompatActivity {
 
         return imageView;
     }
+
+    /*
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        int inSampleSize = 1;
+        if (options.outHeight > reqHeight || options.outWidth> reqWidth) {
+
+            // don't know why we have to divide them by 2 but it's declared like this in documentation
+            final int halfHeight = options.outHeight / 2;
+            final int halfWidth = options.outWidth / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            // (A power of two value is calculated because the decoder uses a final value by rounding down to the nearest power of two,
+            // as per the inSampleSize documentation)
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    private Bitmap decodeBitmapFromResource(Resources res, String imageUrl, int reqWidth, int reqHeight) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+    */
 }
